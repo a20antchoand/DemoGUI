e862320d2648266715e6b6eceb6b10266169a647
a20antchoand <a20antchoand@inspedralbes.cat>
2022-01-26 13:04:34 +0100

    Rotate

--- src/main/java/com/example/demogui/HelloController.java
+++ src/main/java/com/example/demogui/HelloController.java
@@ -2,8 +2,10 @@ package com.example.demogui;
 
 import javafx.beans.Observable;
 import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -21,10 +23,49 @@ public class HelloController implements Initializable {
     private GridPane ekonos_GP;
     @FXML
     private AnchorPane tablero_AnchorPane;
+    @FXML
+    private ImageView rotateImp1;
+    @FXML
+    private ImageView rotatePar1;
+    @FXML
+    private ImageView rotateImp2;
+    @FXML
+    private ImageView rotatePar2;
+    @FXML
+    private ImageView rotateImp3;
+    @FXML
+    private ImageView rotatePar3;
+
+    @FXML
+    private ImageView rotateP1;
+    @FXML
+    private ImageView rotateP2;
+    @FXML
+    private ImageView rotateP3;
+    @FXML
+    private ImageView rotateP4;
+    @FXML
+    private ImageView rotateP5;
+    @FXML
+    private ImageView rotateP6;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+       rotatePar3.setOnMouseClicked((ActionEvent) -> {
+               rotateImp1.setRotate(rotateImp1.getRotate() + 20);
+               rotatePar1.setRotate(rotatePar1.getRotate() - 20);
+               rotateImp2.setRotate(rotateImp2.getRotate() + 20);
+               rotatePar2.setRotate(rotatePar2.getRotate() - 20);
+               rotateImp3.setRotate(rotateImp3.getRotate() + 20);
+               rotatePar3.setRotate(rotatePar3.getRotate() - 20);
+           rotateP1.setRotate(rotateP1.getRotate() + 20);
+           rotateP2.setRotate(rotateP2.getRotate() - 20);
+           rotateP3.setRotate(rotateP3.getRotate() + 20);
+           rotateP4.setRotate(rotateP4.getRotate() - 20);
+           rotateP5.setRotate(rotateP5.getRotate() + 20);
+           rotateP6.setRotate(rotateP6.getRotate() - 20);
+       });
 
     }
 }
