488ed82e16a41ac98ea3c956a84be595d4911877
maurolasala <mlasala@lumsoft.es>
2022-02-01 14:03:51 +0100

    Problema con el tablero

--- src/main/java/com/example/demogui/controladores/ControladorPrincipal.java
+++ src/main/java/com/example/demogui/controladores/ControladorPrincipal.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.awt.*;
 import java.io.IOException;
@@ -19,6 +20,8 @@ import java.util.ResourceBundle;
 public class ControladorPrincipal implements Initializable {
 
     final double TABLERO_ASPECT_RATIO = 0.7244600116754232;
+    final double MARGEN_TABLERO = 90.0;
+    final double MEDIDA_BARRA_STAGE = 20.0;
 
 
     @FXML
@@ -49,27 +52,57 @@ public class ControladorPrincipal implements Initializable {
 
 
     private void redimensionarJuego() {
-        adaptarDimensionPantalla();
-        colocarBotonesSucursales();
+        final double[] medidasTablero = calcularMedidasTablero();
+
+
+        adaptarDimensionPantalla(medidasTablero[0], medidasTablero[1]);
+        colocarBotonesSucursales(medidasTablero[0], medidasTablero[1]);
     }
 
 
-    private void adaptarDimensionPantalla() {
+    private double[] calcularMedidasTablero() {
         Dimension dimensionesPantalla = Toolkit.getDefaultToolkit().getScreenSize();
-        double windowHeight = dimensionesPantalla.getHeight() - 90;
-        double windowWidth = (windowHeight - 20) * TABLERO_ASPECT_RATIO + 300 + 200;
+        double width;
+        double height;
 
 
-        ekonos_GP.setPrefWidth(windowWidth);
-        ekonos_GP.setPrefHeight(windowHeight);
+        height = dimensionesPantalla.getHeight() - 120;
+        width = height * TABLERO_ASPECT_RATIO;
+
+        return new double[] { width, height };
     }
 
 
-    private void colocarBotonesSucursales() {
+    private double obtenerAnchoPanelesLaterales() {
+        double ancho = 0;
+
+
+        for (var panel : ekonos_GP.getChildren())
+            if (!panel.equals(panelTablero)) ancho += ((Pane) panel).getWidth();
+
+
+        return ancho;
+    }
+
+
+    private void adaptarDimensionPantalla(double tableroWidth, double tablerHeight) {
+        ekonos_GP.getScene().getWindow().setWidth(tableroWidth + obtenerAnchoPanelesLaterales());
+        ekonos_GP.getScene().getWindow().setHeight(tablerHeight + MEDIDA_BARRA_STAGE + 100);
+
+
+        panelTablero.setMinWidth(tableroWidth);
+        panelTablero.setPrefWidth(tableroWidth);
+        panelTablero.setMaxWidth(tableroWidth);
+
+        panelTablero.setMinHeight(tablerHeight);
+        panelTablero.setPrefHeight(tablerHeight);
+        panelTablero.setMaxHeight(tablerHeight);
+    }
+
+
+    private void colocarBotonesSucursales(double tableroWidth, double tableroHeight) {
         final double tableroWidth_muestra = 606;
         final double tableroHeight_muestra = 836;
-        final double tableroWidth_actual = panelTablero.getScene().getWidth() - 500;
-        final double tableroHeight_actual = panelTablero.getScene().getHeight();
 
 
         for (var btnSucursal : panelTablero.getChildren()) {
@@ -77,21 +110,27 @@ public class ControladorPrincipal implements Initializable {
             double leftAnchor_actual = AnchorPane.getLeftAnchor(btnSucursal);
             double topAnchor_actual = AnchorPane.getTopAnchor(btnSucursal);
 
-            double leftAnchor_nuevo = tableroWidth_actual * leftAnchor_actual / tableroWidth_muestra;
-            double topAnchor_nuevo = tableroHeight_actual * topAnchor_actual / tableroHeight_muestra;
+            double leftAnchor_nuevo = tableroWidth * leftAnchor_actual / tableroWidth_muestra;
+            double topAnchor_nuevo = tableroHeight * topAnchor_actual / tableroHeight_muestra;
 
 
             // Dimensiones
-            double nuevoWidth = tableroWidth_actual * ((javafx.scene.control.Button) btnSucursal).getWidth() / tableroWidth_muestra;
+            double nuevoWidth = tableroWidth * ((Button) btnSucursal).getWidth() / tableroWidth_muestra;
             double nuevoHeight = nuevoWidth;
 
 
             // Cambio a los nuevos valores
-            ((javafx.scene.control.Button) btnSucursal).setPrefWidth(nuevoWidth);
+            ((Button) btnSucursal).setPrefWidth(nuevoWidth);
             ((Button) btnSucursal).setPrefHeight(nuevoHeight);
             AnchorPane.setLeftAnchor(btnSucursal, leftAnchor_nuevo);
             AnchorPane.setTopAnchor(btnSucursal, topAnchor_nuevo);
         }
+
+
+        System.out.println("Dimensiones pantalla: " + ekonos_GP.getScene().getWindow().getWidth() + " * " + ekonos_GP.getScene().getWindow().getHeight());
+        System.out.println("Dimensiones paneles laterales: " + obtenerAnchoPanelesLaterales());
+        System.out.println("Dimensiones tablero: " + panelTablero.getWidth() + " * " + panelTablero.getHeight());
+
     }
 
 
